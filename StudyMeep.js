fa295bc000fcfa620e2bf7a07ed5598905eeb509
Amanda <amandal8122@gmail.com>
2021-06-14 16:17:30 +0000

    can delete rows, but deletes main row

--- StudyMeep.js
+++ StudyMeep.js
@@ -1,30 +1,33 @@
-const form = document.querySelector('#add-new-task');
+// const form = document.querySelector('#add-new-task');
+// const taskList = document.querySelector('#task-list');
 
-function addTask() {
-    var table = document.getElementById("myTasks");
-    var row = table.insertRow(0);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    var cell4 = row.insertCell(3);
+var button = document.getElementById("add-new-task");
+button.onclick = delRow();
 
-    cell1.innerHTML= document.getElementbyId('cellOne').value;
-    cell2.innerHTML= document.getElementbyId('cellTwo').value;
-    cell3.innerHTML= document.getElementbyId('cellThree').value;
-    cell4.innerHTML= document.getElementbyId('cellFour').value;
 
+
+function delRow(x) {
+    var i = x.rowIndex;
+  document.getElementById("myTasks").deleteRow(i+1);
+// table.deleteRow(i);
 }
 
-form.addEventListener('submit',(e) => {
-    e.preventDefault();
-    db.collection('cafes').add({
-      task: form.task.value,
-      notes: form.notes.value,
-      due: form.due.value,
-      urgency: form.urgency.value
-    });
-})
-form.task.value = '';
-form.notes.value ='';
-form.due.value ='';
-form.urgency.value ='';
+
+// function createTask(doc){
+//     let li = document.createElement('li');
+//     let task = document.createElement('span');
+//     let notes = document.createElement('span');
+//     let due = document.createElement('span');
+//     let urgency = document.createElement('span');
+
+
+
+//     li.appendChild(task);
+//     li.appendChild(notes);
+//     li.appendChild(due);
+//     li.appendChild(urgency);
+//     // li.appendChild(trash);
+
+//     taskList.appendChild();
+
+// }
