90c133905d6c4abd9183bf2793c559805368b0bc
Amanda <amandal8122@gmail.com>
2021-06-15 14:25:43 +0000

    addTask button function and creating editable fields

--- StudyMeep.js
+++ StudyMeep.js
@@ -1,33 +1,18 @@
-// const form = document.querySelector('#add-new-task');
-// const taskList = document.querySelector('#task-list');
-
-var button = document.getElementById("add-new-task");
+var button = document.getElementById("trash-button");
 button.onclick = delRow();
 
+var button2 = document.getElementById("add-new-task");
+button2.onclick = addRow();
 
 
 function delRow(x) {
     var i = x.rowIndex;
-  document.getElementById("myTasks").deleteRow(i+1);
-// table.deleteRow(i);
+  document.getElementById("myTasks").deleteRow(i);
 }
 
+function addRow(){
+  var addRow = document.getElementById("myTasks").insertRow();
+  addRow.innerHTML =
 
-// function createTask(doc){
-//     let li = document.createElement('li');
-//     let task = document.createElement('span');
-//     let notes = document.createElement('span');
-//     let due = document.createElement('span');
-//     let urgency = document.createElement('span');
-
-
-
-//     li.appendChild(task);
-//     li.appendChild(notes);
-//     li.appendChild(due);
-//     li.appendChild(urgency);
-//     // li.appendChild(trash);
-
-//     taskList.appendChild();
-
-// }
+  "<td contenteditable='true'></td> <td contenteditable='true'></td> <td contenteditable='true'></td> <td contenteditable='true'></td> <td           <button id='trash-button' onclick = 'delRow(this)'> <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M3 6v18h18v-18h-18zm5 14c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4-18v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712z'/></svg></td>";
+}
